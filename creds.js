bd5a266e7031c694846b70db86ca0bcbcde66c0c
yatajwale7777 <yatajwale7777@gmail.com>
2025-08-21 16:22:08 +0530

    Update creds.js

--- creds.js
+++ creds.js
@@ -1,16 +1,14 @@
 const { google } = require("googleapis");
 
-if (!process.env.GOOGLE_CREDENTIALS_BASE64) {
-  throw new Error("❌ GOOGLE_CREDENTIALS_BASE64 not found");
-}
-
-const keyFileJson = JSON.parse(
-  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, "base64").toString("utf-8")
+const creds = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, "base64").toString("utf8")
 );
 
-const auth = new google.auth.GoogleAuth({
-  credentials: keyFileJson,
-  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-});
+const auth = new google.auth.JWT(
+  creds.client_email,
+  null,
+  creds.private_key,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
 
 module.exports = auth;
