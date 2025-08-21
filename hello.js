bf9b5b98a6327dbb85498a37fd8f8f075a8f12d5
yatajwale7777 <yatajwale7777@gmail.com>
2025-08-21 15:19:02 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log("Hello from GitHub Actions 🚀");
+console.log("🚀 GitHub Actions Test Start");
+
+for (let i = 1; i <= 5; i++) {
+  console.log(`Line number ${i}`);
+}
+
+console.log("✅ Script successfully executed in GitHub Actions!");
