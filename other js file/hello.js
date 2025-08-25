57ecd233754fd0ed4f22ac2de609059f800ef88e
yatajwale7777 <yatajwale7777@gmail.com>
2025-08-25 13:02:21 +0530

    ok
    ok

--- other js file/hello.js
+++
@@ -1,7 +0,0 @@
-console.log("🚀 GitHub Actions Test Start");
-
-for (let i = 1; i <= 5; i++) {
-  console.log(`Line number ${i}`);
-}
-
-console.log("✅ Script successfully executed in GitHub Actions!");
--- other js file/service_details.js
+++
@@ -1,7 +0,0 @@
-// service_details.js
-export const SHEET_ID = "1vi-z__fFdVhUZr3PEDjhM83kqhFtbJX0Ejcfu9M8RKo";  // aapka Google Sheet ID
-
-export const SHEET_RANGE = "Sheet1!A1"; // default range
-
-export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL;
-export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n');
